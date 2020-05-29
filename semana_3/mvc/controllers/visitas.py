914bfbb736b367dd82bcd98f1ae9dd634bd1f6dd
Kevinchch12 <1718110380@utectulancingo.edu.mx>
2020-05-29 06:52:38 +0000

    Creacion de cookies con fecha y hora

---
+++ semana_3/mvc/controllers/visitas.py
@@ -0,0 +1,29 @@
+import web 
+import time
+
+
+class Visitas:
+    def GET(self, nombre):
+      try:
+          cookie = web.cookies()
+          visitas = "0"
+          print(cookie)
+          if nombre:
+            web.setcookie("nombre",nombre,expires="",domain=None)
+          else:
+            nombre = " anonimo"
+            web.setcookie("nombre",nombre,expires="",domain=None)
+          if cookie.get("visitas"):
+              visitas = int(cookie.get("visitas"))
+              visitas += 1
+              web.setcookie("visitas", str(visitas), expires="", domain=None)
+          else:
+              web.setcookie("visitas", str(1), expires="", domain=None)
+              visitas = "1"
+
+
+          return "Visitas " + str(visitas) + " nombre" + nombre + " Hora:" + time.strftime("%H:%M:%S") + " Fecha: " + time.strftime("%d:%m:%Y")
+          
+      except Exception as e:
+          return "Error " + str(e.args)
+        
